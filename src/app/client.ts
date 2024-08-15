fe99ee7e08a3a07b9bc4a06fe9a169558669af9d
alperenbekci <alperenbekci@hotmail.com>
2024-08-15 22:12:49 +0300

    env update

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,6 +1,6 @@
 import { createThirdwebClient } from "thirdweb";
 
-const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
+const CLIENT_ID = process.env.NEXT as string;
 
 export const client = createThirdwebClient({
   clientId: CLIENT_ID,
